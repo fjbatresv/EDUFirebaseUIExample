ca526465f142df3f133b72d2d7d74b8159a7453a
Javier Batres <fjbatresv@gmail.com>
2018-07-21 09:33:39 -0600

    Update for gradle 4.4

--- app/src/main/java/edu/edutec/fjbatresv/firebaseuiexample/LoginActivity.java
+++ app/src/main/java/edu/edutec/fjbatresv/firebaseuiexample/LoginActivity.java
@@ -4,23 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth auth;
+    private DatabaseReference ref;
     private static final int RC_SIGN_IN = 100;
+    @BindView(R.id.edit)
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
         auth = FirebaseAuth.getInstance();
+        ref = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
@@ -45,14 +59,22 @@ public class LoginActivity extends AppCompatActivity {
         return providers;
     }
 
+    @OnClick(R.id.btn)
+    public void sendDb(){
+        Date date = new Date();
+        String key = ref.child("posts").push().getKey();
+        ref.child("posts").child(key)
+                .setValue(new Post(edit.getText().toString(), date.getTime()));
+        edit.setText(null);
+        Toast.makeText(this, "Enviado a la DB", Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_OK) {
                 Log.e("SIGN_IN_OK", String.valueOf(auth.getCurrentUser().getUid()));
-                Log.e("SIGN_OUT", auth.getCurrentUser().getUid());
-                auth.signOut();
             }else{
                 Log.e("SIGN_IN_FAIL", String.valueOf(data.getDataString()));
             }
---
+++ app/src/main/java/edu/edutec/fjbatresv/firebaseuiexample/Post.java
@@ -0,0 +1,37 @@
+package edu.edutec.fjbatresv.firebaseuiexample;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by javie on 4/21/2018.
+ */
+
+public class Post implements Serializable {
+    private String text;
+    private long time;
+
+    public Post() {
+    }
+
+    public Post(String text, long time) {
+        this.text = text;
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
